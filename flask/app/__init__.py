e683871c323faeca34e7633f7c184ab9eea579b6
NTCHz <Nonny.1122@hotmail.com>
2025-02-01 15:34:40 +0700

    Sturcture

---
+++ flask/app/__init__.py
@@ -0,0 +1,35 @@
+import os
+from flask import Flask, request, redirect
+from werkzeug.debug import DebuggedApplication
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__, static_folder='static')
+app.url_map.strict_slashes = False
+
+app.jinja_options = app.jinja_options.copy()
+app.jinja_options.update({
+    'trim_blocks': True,
+    'lstrip_blocks': True
+})
+
+app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = \
+    '0adc9fde45a2d07af00cc2f2ec8250a267dd26a3d217228c'
+app.config['JSON_AS_ASCII'] = False
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "sqlite://")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+if app.debug:
+    app.wsgi_app = DebuggedApplication(app.wsgi_app, evalex=True)
+
+# Creating an SQLAlchemy instance
+db = SQLAlchemy(app)
+
+@app.before_request
+def remove_trailing_slash():
+   # Check if the path ends with a slash but is not the root "/"
+    if request.path != '/' and request.path.endswith('/'):
+        return redirect(request.path[:-1], code=301)
+
+from app import views  # noqa
