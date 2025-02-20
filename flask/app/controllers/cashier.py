60b34bbe8c386779335b1d30fc5f8a01d3435515
kittapas-g <kittapas_g@cmu.ac.th>
2025-02-20 15:35:01 +0700

    setting_up_cashier_page

---
+++ flask/app/controllers/cashier.py
@@ -0,0 +1,14 @@
+# from app.controllers import ctable_mange
+from app.controllers import table_manage
+from app.controllers import menu_manage
+from app.controllers import employee_manage
+from app.controllers import order_manage
+
+from app.controllers.Dashboard import Revenue
+
+from flask import Flask, render_template
+from app import app
+
+@app.route('/cashier')
+def cashier_index():
+    return render_template('cashier_page/index.html')   
