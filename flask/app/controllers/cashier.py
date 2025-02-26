87643741944f1184ad162c49194246eea295c457
kittapas-g <kittapas_g@cmu.ac.th>
2025-02-26 14:07:43 +0700

    feat: invoice page done

--- flask/app/controllers/cashier.py
+++ flask/app/controllers/cashier.py
@@ -13,3 +13,8 @@ def invoice():
     table_id = request.args.get('table_id')
     qrcode = request.args.get('qrcode')
     return render_template('cashier_page/invoice.html', table_id=table_id, qrcode=qrcode)
+
+
+
+
+# @app.route('/cashier/invoice/download')
