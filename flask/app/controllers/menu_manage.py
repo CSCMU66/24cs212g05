2aa669c44069381063890f2d4cd3471fff886a18
Thanarak-q <Thanarak_ka@cmu.ac.th>
2025-02-26 18:22:04 +0700

    perf: a lot

--- flask/app/controllers/menu_manage.py
+++ flask/app/controllers/menu_manage.py
@@ -16,7 +16,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/menu', methods=('GET', 'POST'))
+
 @app.route('/menu', methods=('GET', 'POST'))
 def menu_list():
     if request.method == 'POST':
