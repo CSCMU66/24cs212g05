51cd735cc96def9152573c73ae116219f4a07090
kittapas-g <kittapas_g@cmu.ac.th>
2025-02-27 17:13:56 +0700

    add(invoice): not complete

--- flask/app/controllers/cashier.py
+++ flask/app/controllers/cashier.py
@@ -4,11 +4,22 @@ from app.controllers.role_controller import roles_required
 
 from app import app
 
+
+
+
 @app.route('/cashier')
 @login_required
 @roles_required('Admin', 'Cashier')
 def cashier_index():
     return render_template('cashier_page/index.html')
+
+
+
+@app.route('/cashier/qrcode')
+def qrcode():
+    table_id = request.args.get('table_id')
+    qrcode = request.args.get('qrcode')
+    return render_template('cashier_page/qrcode.html', table_id=table_id, qrcode=qrcode)
 
 
 
@@ -18,9 +29,4 @@ def cashier_index():
 def invoice():
     table_id = request.args.get('table_id')
     qrcode = request.args.get('qrcode')
-    return render_template('cashier_page/order_bill.html', table_id=table_id, qrcode=qrcode)
-
-
-
-
-# @app.route('/cashier/invoice/download')
+    return render_template('cashier_page/invoice.html', table_id=table_id, qrcode=qrcode)
